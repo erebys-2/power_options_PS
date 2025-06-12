b7eebec703bd1ebb3dbce73483338a1dda36f428
erebys-2 <168375967+erebys-2@users.noreply.github.com>
2025-06-11 19:14:02 -0500

    refined dictionary + add README
    -power profile GUIDs and names are no longer hard-coded

---
+++ README.txt
@@ -0,0 +1,5 @@
+I didn't realize I could make a shortcut to Power Options or just enter powercfg.cpl into CMD
+to achieve the same functionality as this script.
+
+I suppose this could be paired with a hotkey to quickly set power profile with keyboard input.
+https://stackoverflow.com/questions/39677467/launch-powershell-script-with-keyboard-hotkey
---
+++ pp_shell_script.ps1
@@ -0,0 +1,34 @@
+Write-Output "Power Profiles are listed, * = Active"
+# Create dictionary
+$powerGuidDict = [System.Collections.Specialized.OrderedDictionary]::new()
+$i = 0
+foreach ($pp in (powercfg /l)) {#loop through powercfg profiles, works like a list of strings
+    if($i -gt 2) #>2, skip header lines; extract data
+    {
+        $key = $i - 2
+        $ppstr = [string] $pp
+        $guid_substr = $ppstr.Substring(19,36)
+        $name_substr = $ppstr.Substring(57)
+        
+        $powerGuidDict.Add([string] $key, $guid_substr) #populate dictionary
+        $formatted = $key,$name_substr -join " " #create and display formatted string      
+        Write-Output $formatted
+    }
+    $i = $i + 1
+}
+
+# Display the dictionary
+#$powerGuidDict
+
+#user input
+$ppkey = Read-Host 'Enter a number'
+$ppkey
+
+if ($powerGuidDict.Contains($ppkey)) {
+    powercfg /S $powerGuidDict[$ppkey] #set power profile if found
+    Write-Output "Power Profile Set."
+    powercfg /L
+} else {
+    Write-Output "Key '$ppkey' does not exist. Opening powercfg.cpl..."
+    powercfg.cpl
+}
